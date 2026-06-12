d62363e6044230ac07c97131024ade9d0dbb211a
Welpyes <helloilikeoppai@protonmail.com>
2026-06-12 19:58:11 +0800

    adjust relax hit window to better match lazer

--- src/ru/nsu/ccfit/zuev/osu/game/GameObject.java
+++ src/ru/nsu/ccfit/zuev/osu/game/GameObject.java
@@ -119,7 +119,7 @@ public abstract class GameObject {
                         }
 
                         // Case 2
-                        if (objectElapsedTime >= 0 && isHit) {
+                        if (objectElapsedTime >= 0.005 && isHit) {
                             return event;
                         }
                     }
